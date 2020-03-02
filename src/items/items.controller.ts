ea7b6fde084d9f530be4f21bb815652808a8b8d1
Rémi Arquevaux <r.arquevaux@gmail.com>
2020-03-02 22:21:30 +0100

    add items endpoint

---
+++ src/items/items.controller.ts
@@ -0,0 +1,17 @@
+import { Get, Post, Body, Controller } from '@nestjs/common';
+import { ItemsService } from './items.service';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Get()
+  async findAll(): Promise<string[]> {
+    return this.itemsService.findAll();
+  }
+
+  @Post()
+  async create(@Body() item: string) {
+    this.itemsService.create(item);
+  }
+}
