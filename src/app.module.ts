984291927c730bea480fcdd32cff609af8b33bdb
rémi arquevaux <r.arquevaux@gmail.com>
2020-03-22 18:22:57 +0100

    import TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,28 @@ import { Module } from '@nestjs/common';
 import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
 import { ShoppingCartController } from './shopping-cart/shopping-cart.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from './items/item.interface';
+//import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // TO DO -> use ConfigModule to pass config
+    // ConfigModule exposes a ConfigService wich loads the .env file
+    //ConfigModule.forRoot(),
+    // TO DO -> create an ormconfig.json file
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: 'postgres',
+      port: 5432,
+      username: 'admin',
+      password: 'nesta',
+      database: 'items',
+      entities: [Item],
+      synchronize: true,
+
+    }),
+  ],
   controllers: [ItemsController, ShoppingCartController],
   providers: [ItemsService],
 })
