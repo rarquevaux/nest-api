f5a6b05f75cfe23f22d7c0d82685c52c148553a4
rémi arquevaux <r.arquevaux@gmail.com>
2020-04-13 17:32:15 +0200

    item service tests

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -8,7 +8,9 @@ describe("-- Items Service --", () => {
     let itemService: ItemsService;
     let module: TestingModule;
     let itemRepositoryMock: MockType<Repository<Item>>;
-    const mockNumberToSatisfyParameters = 0;
+    const item = {id: 1, name: 'Melanzane',price: 6};
+    const item2 = {id: 2, name: 'Anchovy',price: 2};
+    const item3 = {id: 3, name: 'Pepperoni',price: 4};
 
     beforeAll(async () => {
       module = await Test.createTestingModule({
@@ -24,30 +26,62 @@ describe("-- Items Service --", () => {
 
     describe('createItem', () => {
         it('should save an item in the database', async () => {
-          const params = {
-            name: 'name',
-            price: 6,
-          };
-    
+          
           itemRepositoryMock.save = jest.fn();
-          await itemService.createItem(params);
+          await itemService.createItem(item);
     
           expect(itemRepositoryMock.save).toHaveBeenCalledWith(
-            Object.assign(new Item(), params),
-          );
-        });
+            Object.assign(new Item(), item),
+            );
+          });
       });
 
-});
+    describe('findOne', () => {
+        it('should return the item which id was passed', () => {
+          
+          itemRepositoryMock.findOne.mockReturnValue(item);
+          return itemService.findOne(item.id).then(data => {
+            expect(data).toEqual(item);
+            });
+        });
+        
+        it('should have been called with id', async () => {
+          itemService.findOne(item.id);
+          expect(itemRepositoryMock.findOne).toHaveBeenCalledWith(item.id);
+        });
+          
 
+      });
+
+    describe('findAll', () => {
+        it('should return all items', async () => {
+
+          const items: Item[] = [];
+          items.push(item);
+          items.push(item2);
+          items.push(item3);
+
+          itemRepositoryMock.find.mockReturnValue(items);
+          return itemService.findAll().then(data => {
+            expect(data).toEqual(items);
+          });
+          
+        });
+
+        it('should have been called', async () => {
+          itemService.findAll;
+          expect(itemRepositoryMock.find).toHaveBeenCalled();
+        });
+      });
+  });
 
 
 // @ts-ignore
 export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(() => ({
-    findOne: jest.fn(),
+    findOne: jest.fn(entity => entity),
     find: jest.fn(),
     save: jest.fn()
    }));
 export type MockType<T> = {
     [P in keyof T]: jest.Mock<{}>;
-};
+   };
