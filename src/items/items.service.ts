3747c2f8d9dc8524e531bae1e294856909a9a1de
rémi arquevaux <r.arquevaux@gmail.com>
2020-04-13 16:41:32 +0200

    update items service

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -10,26 +10,30 @@ export class ItemsService {
         private itemsRepository: Repository<Item>,
     ){}
  
-    //findAll returns an array with all the items
+    /**
+    * findAll
+    * @return an array with all the Items
+    * */
     async findAll(): Promise<Item[]> {
-        const allItem: Item[] = await this.itemsRepository.find();
-        return allItem;
+        return this.itemsRepository.find();
     }
 
-    //findOne returns an item
-    //findOne take an item id as an argument
+    /**
+    * findOne 
+    * @param a number
+    * @return an Item
+    * */
     async findOne(id: number): Promise<Item>{
-        const item: Item = await this.itemsRepository.findOne(id);
-        return item;
+        return this.itemsRepository.findOne(id);
     } 
 
-    //TODO => avoid the creation of the same item with multiple ids
-
-    //createItem saves a new item in the database
-    //createItem takes an item as an argument
+    /**
+    * createItem 
+    * @param an Item
+    * @return an Item
+    * */
     async createItem(item: Item): Promise<Item>{
-        await this.itemsRepository.save(item);
-        return item;
+       return this.itemsRepository.save(item);
     }
 
 }
