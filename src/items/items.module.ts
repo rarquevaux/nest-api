72272c457c2d808ff25f9ac1f386408c35d99ba4
rémi arquevaux <r.arquevaux@gmail.com>
2020-04-17 14:16:47 +0200

    add ClientsModule ms-data-sharing in imports

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -3,9 +3,22 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { ItemsService } from './items.service';
 import { ItemsController } from './items.controller';
 import { Item } from './item.entity';
+import { ClientsModule, Transport } from "@nestjs/microservices";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Item])],
+    imports: [
+        // TO DO use a custom provider to fetch the configuration from a service 
+        ClientsModule.register([
+            {
+                name: "ms-data-sharing",
+                transport: Transport.TCP,
+                options: {
+                      host: "127.0.0.1",
+                      port: 3001
+                    }
+                }
+            ]),
+        TypeOrmModule.forFeature([Item])],
     providers: [ItemsService],
     controllers: [ItemsController],
 })
